cd15fa73613b90daf24bfe6f00b585de29c7697d
ugsdf <johannes.stephan@student.kit.edu>
2023-01-24 21:15:15 +0100

    Added Test-cases

--- src/test/java/edu/kit/recipe/recipebackend/RecipebackendApplicationTests.java
+++ src/test/java/edu/kit/recipe/recipebackend/RecipebackendApplicationTests.java
@@ -1,13 +1,29 @@
 package edu.kit.recipe.recipebackend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RecipebackendApplicationTests {
 
+	@Autowired
+	MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	void shouldReturnHelloWorld() throws Exception {
+		mvc.perform(get("/")).andExpect(status().isOk());
+	}
+
+	@Test
+	void shouldReturnNotFound() throws Exception {
+		mvc.perform(get("/notfound")).andExpect(status().isNotFound());
 	}
 
 }
