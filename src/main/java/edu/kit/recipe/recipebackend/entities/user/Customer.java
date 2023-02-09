b0cfc0c986c17685fb5966bc897b745a81b13df6
Johannes Stephan <111685646+johan-stph@users.noreply.github.com>
2023-02-09 16:52:23 +0100

    Added user-verification (#28)
    * Added user-verification
    
    * Removed Builder and added AllArgsConstructor

---
+++ src/main/java/edu/kit/recipe/recipebackend/entities/user/Customer.java
@@ -0,0 +1,44 @@
+package edu.kit.recipe.recipebackend.entities.user;
+
+
+import edu.kit.recipe.recipebackend.entities.Recipe;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Builder
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    @Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true)
+    private String email;
+
+    @OneToMany(targetEntity = Recipe.class, cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private List<Recipe> recipe;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Customer customer = (Customer) o;
+        return id != null && Objects.equals(id, customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
