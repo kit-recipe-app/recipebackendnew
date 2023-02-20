a445023b8bd5d2ba956567cd71e520aecc293aa2
johan-stph <111685646+johan-stph@users.noreply.github.com>
2023-02-20 13:01:35 +0100

    Changed primitive type

--- src/main/java/edu/kit/recipe/recipebackend/entities/Recipe.java
+++ src/main/java/edu/kit/recipe/recipebackend/entities/Recipe.java
@@ -29,7 +29,7 @@ public class Recipe {
     private UUID id;
     private String name;
 
-    private boolean isPublic = true;
+    private Boolean isPublic = true;
 
 
     private String description;
