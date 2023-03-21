4a4b224eb754b9f797c4554adaec9fab6ff69356
johan-stph <111685646+johan-stph@users.noreply.github.com>
2023-03-21 15:53:22 +0100

    added transactional

--- src/main/java/edu/kit/recipe/recipebackend/repository/ImageRepository.java
+++ src/main/java/edu/kit/recipe/recipebackend/repository/ImageRepository.java
@@ -1,12 +1,14 @@
 package edu.kit.recipe.recipebackend.repository;
 
 import edu.kit.recipe.recipebackend.entities.image.ImageData;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Transactional
 public interface ImageRepository extends JpaRepository<ImageData, UUID> {
     Optional<ImageData> findByName(String fileName);
 
--- src/main/java/edu/kit/recipe/recipebackend/repository/RecipeRepository.java
+++ src/main/java/edu/kit/recipe/recipebackend/repository/RecipeRepository.java
@@ -2,11 +2,13 @@ package edu.kit.recipe.recipebackend.repository;
 
 
 import edu.kit.recipe.recipebackend.entities.Recipe;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Transactional
 public interface RecipeRepository extends JpaRepository<Recipe, UUID> {
     List<RecipeInfo> findByIsPublicTrue();
 }
