3926591ff8c5123f52732daf3f658d3f0c1d4bb5
Johannes Stephan <111685646+johan-stph@users.noreply.github.com>
2023-03-25 20:37:15 +0100

    Added more integration Tests (#50)

--- src/test/java/integTests/BackendITests.java
+++ src/test/java/integTests/BackendITests.java
@@ -20,9 +20,11 @@ import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
@@ -33,12 +35,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-/**
- *
- * @author Josh Cummings
- */
+
 @SpringBootTest(classes = RecipebackendApplication.class)
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class BackendITests {
 
 	String bennyOauth2Token =
@@ -187,6 +188,14 @@ class BackendITests {
 				.andReturn();
 		String content1 = mvcResult1.getResponse().getContentAsString();
 		assertEquals(content1, "{\"name\":\"Benny\",\"email\":\"greendr4gon@protonmail.com\"}", false);
+	}
+
+	@Test
+	void addInvalidTags() throws Exception {
+		this.mvc.perform(post("/api/v1/tags").with(bearerToken(this.bennyOauth2Token))
+						.contentType("application/json")
+						.content("{\"name\":\"v1\"}"))
+				.andExpect(status().isBadRequest());
 	}
 
 
